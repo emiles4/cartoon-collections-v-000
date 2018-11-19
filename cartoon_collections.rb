f5ed87a96823cfa2c1c96f0a67c47535b8a4e6de
Emily Miles <emiles4@gmail.com>
2018-11-19 05:05:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,12 @@ def long_planeteer_calls(calls)
   calls.any?{|word| word.length>4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(list)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  for i in cheese_types
+  	if list.include?(i)
+  		return i
+  	end
+  end
+  return
 end
